f9378ab15ce4bf9243d75e6645a8a17ed34a3d9e
Dan Sutton <danielsutton01@gmail.com>
2016-07-26 23:41:19 -0500

    Bump jdbc to current 0.6.1

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject simple-query "0.1.0"
+(defproject simple-query "0.1.2"
   :description "Easy way to add sql to a project"
   :url "https://github.com/dpsutton/simple-query"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.microsoft/sqljdbc4 "3.0"]
-                 [org.clojure/java.jdbc "0.4.2"]])
+                 [org.clojure/java.jdbc "0.6.1"]])
