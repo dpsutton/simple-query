8cefdc2eb60626a12b039ab9cbe5dcb80f13a066
Dan Sutton <danielsutton01@gmail.com>
2016-07-26 22:04:07 -0500

    Add description and version number

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject simple-query "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject simple-query "0.1.0"
+  :description "Easy way to add sql to a project"
+  :url "https://github.com/dpsutton/simple-query"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
